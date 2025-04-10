a1d7f51fc4ecc67dd20e2ebfe06736d859a28c24
Andrej Bočkaj <106064818+BockajAndrej@users.noreply.github.com>
2025-04-10 14:14:10 +0200

    Created debug.cs class

---
+++ IPK25-CHAT/src/Debug.cs
@@ -0,0 +1,11 @@
+﻿namespace IPK25_CHAT;
+
+public static class Debug
+{
+    public static bool isEnabled = true;
+    public static void StdErrWriteLine(string line)
+    {
+        if (isEnabled)
+            Console.Error.WriteLine(line);
+    }
+}
