5f4ca8cfe2428e1e65b366754fea4dc568653b57
Andrej Bočkaj <106064818+BockajAndrej@users.noreply.github.com>
2025-04-19 23:25:41 +0200

    feat: warning about userproperty initializations was removed

--- IPK25-CHAT/src/structs/UserProperty.cs
+++ IPK25-CHAT/src/structs/UserProperty.cs
@@ -8,17 +8,17 @@ public struct UserProperty()
     public int MessageId = 0;
 
     [RegularExpression(@"^[a-zA-Z0-9_]{1,20}$")]
-    public string Username { get; set; }
-    
+    public string Username { get; set; } = null!;
+
     [RegularExpression(@"^[a-zA-Z0-9_]{1,20}$")]
-    public string ChanelId;
+    public string ChanelId = null!;
     
     [RegularExpression(@"^[a-zA-Z0-9_]{1,128}$")]
-    public string Secret;
+    public string Secret = null!;
     
     [RegularExpression(@"^[\x21-\x7E]{1,20}$")]
-    public string DisplayName;
+    public string DisplayName = null!;
     
     [RegularExpression(@"^[\x0A\x20-\x7E]{1,6000}$")]
-    public string MessageContent;
+    public string MessageContent = null!;
 }
